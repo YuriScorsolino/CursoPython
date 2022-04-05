f899d50fe2326956288d4f28cd94d62d68a24558
Yuri Scorsolino Esmerol <yuriscorsolino@hotmail.com>
2022-04-04 22:51:41 -0300

    desafio

--- desafio/desafio.py
+++ desafio/desafio.py
@@ -5,5 +5,5 @@ peso = 80
 ano_atual = 2022
 nascimento = ano_atual - idade
 imc = peso / altura ** 2
-print(f'{nome} tem {idade} anos, {altura} de altura e pesa {peso}kg \no IMC de {nome} é {imc:.2f} \n{nome} nasceu em {nascimento} ')
-
+print(f"{nome} tem {idade} anos, {altura} de altura e pesa {peso}kg \no IMC de {nome} é {imc:.2f} \n{nome} nasceu e"
+      f" {nascimento} ")
