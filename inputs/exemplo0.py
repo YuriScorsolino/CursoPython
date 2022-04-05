ed6b2d4cab70460b268ffe4e19a58d36099280de
Yuri Scorsolino Esmerol <yuriscorsolino@hotmail.com>
2022-04-04 23:08:46 -0300

    exemplo

---
+++ inputs/exemplo0.py
@@ -0,0 +1,7 @@
+nome = input('qual seu nome? ')
+idade = input("qual a sua idade? ")
+
+ano_nascimento = 2022-int(idade)
+
+print(f'{nome} tem {idade} anos'
+      f'{nome} nasceu em {ano_nascimento}')
---
+++ inputs/exemplo2.py
@@ -0,0 +1,4 @@
+numero_1 = input('Digite um numero: ')
+numero_2 = input('Digite outro numero: ')
+
+print(int(numero_1) + int(numero_2))
