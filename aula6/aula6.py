21dc77fbf19d54fe955a3fdbbf37873aa77e34b4
Yuri Scorsolino Esmerol <yuriscorsolino@hotmail.com>
2022-04-04 22:17:16 -0300

    aula4

---
+++ aula6/aula6.py
@@ -0,0 +1,10 @@
+"""
+"""
+nome = 'Yuri Scorsolino'
+Idade = 22
+Altura = 1.80
+e_maior = Idade > 18
+peso = 80
+imc = peso / Altura ** 2
+print(' Ola', nome, 'voce tem', Idade, 'anos, sua altura é ', Altura, 'voce é maior de idade?', e_maior, 'e voce pesa ', peso, 'quilos seu imc é', imc)
+print(type("""3"""))
