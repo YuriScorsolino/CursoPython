0e9bd222583c81cbd9f9531b14f49542b13f00ad
Yuri Scorsolino Esmerol <yuriscorsolino@hotmail.com>
2022-04-04 21:37:56 -0300

    aula4

--- aula4/aula4.py
+++ aula4/aula4.py
@@ -5,8 +5,20 @@ str- string textos "assim" 'assim'
 int- inteiro 123456 0 -10 -20
 float- real/ponto flutuante 10.50 1.5
 bool- booleano/logico true/false
-"""
+
 print('Luiz', type('Luiz'))
 print(10, type(10))
 print(20.40, type(20.40))
 print(10==10, type(10==10))
+"""
+#String: nome
+print('Yuri', type('Yuri'))
+
+#Idade: int
+print(22, type(22))
+
+#Altura: float
+print(1.79, type(1.79))
+
+#è maior de idade 10 > 20
+print(22 > 18, type(22))
---
+++ aula5/aula5.py
@@ -0,0 +1,17 @@
+"""
++
+-
+*
+/
+print('Multiplicação', 10*10)
+print('Adição', 10+10)
+print('Subtração', 10-5)
+print('Divisão', 10/2)
+//
+**
+%
+()
+"""
+print(10 // 3)
+print(10 % 3)
+print((5+2)*10)
