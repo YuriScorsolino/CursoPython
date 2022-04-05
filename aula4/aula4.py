5ddb0ad6037086fd7c2083fd224ccca4e623bf5b
Yuri Scorsolino Esmerol <yuriscorsolino@hotmail.com>
2022-04-04 21:07:43 -0300

    aula4

--- aula4/aula4.py
+++ aula4/aula4.py
@@ -6,3 +6,7 @@ int- inteiro 123456 0 -10 -20
 float- real/ponto flutuante 10.50 1.5
 bool- booleano/logico true/false
 """
+print('Luiz', type('Luiz'))
+print(10, type(10))
+print(20.40, type(20.40))
+print(10==10, type(10==10))
