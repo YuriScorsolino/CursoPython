d063b1f35a1996a51423fc7718c4aecacaeb80ec
Yuri Scorsolino Esmerol <yuriscorsolino@hotmail.com>
2022-04-05 20:29:57 -0300

    novas aulas

---
+++ QuantidadeCaracteres/caracteres.py
@@ -0,0 +1,12 @@
+'''usuario= input('Digite seu usuario')
+qtd_caracteres = len(usuario)
+
+if qtd_caracteres < 6:
+    print('Voce precisa digitar pelo menos 6 caracteres')
+else:
+    print('voce foi cadastrado')'''
+
+'''string1 = input('Digite alguma coisa: ')
+string2 = input('Digite outra coisa:')
+
+print(f'a quantidade total de caractere digitado foi {len(string2) + len(string2)}')'''
---
+++ placeholder/place.py
@@ -0,0 +1,5 @@
+valor = False
+if valor:
+    pass #  segurar o lugar para digitar o codigo futuramente
+else:
+    print('Tchau')
