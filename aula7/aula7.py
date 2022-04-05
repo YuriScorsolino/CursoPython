84ae3e0fe478fac7f476bd32421ceee7dba9742c
Yuri Scorsolino Esmerol <yuriscorsolino@hotmail.com>
2022-04-04 22:42:01 -0300

    desafio

---
+++ aula7/aula7.py
@@ -0,0 +1,10 @@
+"""
+"""
+nome = 'Yuri Scorsolino'
+Idade = 22
+Altura = 1.80
+e_maior = Idade > 18
+peso = 80
+imc = peso / Altura ** 2
+print(f'ola {nome}, voce tem {Idade} anos, sua altura é {Altura:.2f}, voce é maior de idade? {e_maior}, seu peso é {peso}, seu imc é {imc:.2f}')
+print('{} tem {} anos de idade e seu imc é {:.2f}'.format(nome, Idade, imc))
---
+++ desafio/desafio.py
@@ -0,0 +1,9 @@
+nome = 'Yuri Scorsolino'
+idade = 22
+altura = 1.80
+peso = 80
+ano_atual = 2022
+nascimento = ano_atual - idade
+imc = peso / altura ** 2
+print(f'{nome} tem {idade} anos, {altura} de altura e pesa {peso}kg \no IMC de {nome} é {imc:.2f} \n{nome} nasceu em {nascimento} ')
+
